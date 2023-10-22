76144e4b94c27d987f608b8d9046f1238e08d40a
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-22 17:57:49 +0900

    feat - 출력 메시지

---
+++ src/Messages.js
@@ -0,0 +1,11 @@
+/**
+ * 프로그램 실행 중 출력되는 메세지
+ */
+export const Messages = Object.freeze({
+  GAME_START_MESSAGE: "숫자 야구 게임을 시작합니다.",
+  INPUT_MESSAGE: "숫자를 입력해주세요 : ",
+  INPUT_INVALID_FORMAT_MESSAGE: "서로 다른 3개의 숫자를 입력해야합니다.",
+  SUCCESS_MESSAGE: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  END_SELECT_MESSAGE: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  END_SELECT_INVALID_FORMAT_MESSAGE: "1 또는 2를 입력해주세요.",
+});
