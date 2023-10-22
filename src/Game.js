52eac50f2586feb1ec833279150fb82377a26329
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-22 18:16:43 +0900

    feat - game 메서드 진행시 입력 및 입력비교 추가

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,5 @@
 import { Answer } from "./Model/Answer";
-
+import { IO } from "./View/IO";
 /**
  * 게임 진행과 관련된 클래스
  */
@@ -8,5 +8,11 @@ export class Game {
   async game() {
     const answer = new Answer();
     const io = new IO();
+
+    //? 올바른 형식(3자리 숫자)이 입력되었다면
+    if (io.getInput()) {
+      //? 사용자 입력과 정답을 비교합니다
+      answer.compareAnswer(io.userInput);
+    }
   }
 }
